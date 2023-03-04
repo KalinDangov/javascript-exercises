cc46b1483cf7f1f5c66cf5446f6085ed13d5e6c3
Kalin Dangov <kalindangov@gmail.com>
2023-03-05 00:10:45 +0200

    forth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...remove) {
+  let newArray = [];
+  for (const arr of array) {
+    if (!remove.includes(arr)) {
+      newArray.push(arr);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
