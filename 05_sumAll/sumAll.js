44ce0011084cc111931fc633ed9e4a7005c5aec0
Kalin Dangov <kalindangov@gmail.com>
2023-03-05 18:10:01 +0200

    fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (fromNum, toNum) {
+  let sum = 0;
+  if (!Number.isInteger(fromNum) || !Number.isInteger(toNum)) return "ERROR";
+  if (fromNum < 0 || toNum < 0) return "ERROR";
+  if (fromNum > toNum) {
+    const stored = toNum;
+    toNum = fromNum;
+    fromNum = stored;
+  }
+  for (i = fromNum; i <= toNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
